a3fe641882f1d9f1b76659d71cbdfd64fbb565c7
baixinsui <baixinsui@outlook.com>
2024-10-11 16:52:29 +0800

    Avoid long waiting times when GitHub API calls exceed the rate limit (#67)

--- src/main/java/org/eclipse/xpanse/tofu/maker/opentofu/tool/OpenTofuVersionsFetcher.java
+++ src/main/java/org/eclipse/xpanse/tofu/maker/opentofu/tool/OpenTofuVersionsFetcher.java
@@ -5,16 +5,20 @@
 
 package org.eclipse.xpanse.tofu.maker.opentofu.tool;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
+import javax.annotation.Nonnull;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.xpanse.tofu.maker.models.exceptions.InvalidOpenTofuToolException;
 import org.kohsuke.github.GHRepository;
 import org.kohsuke.github.GHTag;
 import org.kohsuke.github.GitHub;
 import org.kohsuke.github.GitHubBuilder;
+import org.kohsuke.github.GitHubRateLimitHandler;
 import org.kohsuke.github.PagedIterable;
+import org.kohsuke.github.connector.GitHubConnectorResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
@@ -46,7 +50,10 @@ public class OpenTofuVersionsFetcher {
             backoff = @Backoff(delayExpression = "${spring.retry.delay-millions}"))
     public Set<String> fetchAvailableVersionsFromOpenTofuWebsite() throws Exception {
         Set<String> allVersions = new HashSet<>();
-        GitHub gitHub = new GitHubBuilder().withEndpoint(openTofuGithubApiEndpoint).build();
+        GitHub gitHub = new GitHubBuilder()
+                .withEndpoint(openTofuGithubApiEndpoint)
+                .withRateLimitHandler(getGithubRateLimitHandler())
+                .build();
         GHRepository repository = gitHub.getRepository(openTofuGithubRepository);
         PagedIterable<GHTag> tags = repository.listTags();
         tags.forEach(tag -> {
@@ -76,6 +83,20 @@ public class OpenTofuVersionsFetcher {
         log.info("Get default versions: {} from OpenTofu versions config value: {}",
                 defaultVersions, defaultVersionsString);
         return defaultVersions;
+    }
+
+    private GitHubRateLimitHandler getGithubRateLimitHandler() {
+        return new GitHubRateLimitHandler() {
+            @Override
+            public void onError(@Nonnull GitHubConnectorResponse response) throws IOException {
+                String limit = response.header("X-RateLimit-Limit");
+                String remaining = response.header("X-RateLimit-Remaining");
+                String reset = response.header("X-RateLimit-Reset");
+                String errorMsg = String.format("GitHub API rate limit exceeded. "
+                        + "Rate limit: %s, remaining: %s, reset time: %s", limit, remaining, reset);
+                throw new IOException(errorMsg);
+            }
+        };
     }
 
 }
