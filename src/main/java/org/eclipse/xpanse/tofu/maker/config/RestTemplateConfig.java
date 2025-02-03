fcf7049662b621133f6300b40b7864b55f061065
Bardan Putra Prananto <bppdanto@gmail.com>
2025-02-03 11:31:51 +0300

    add RestTemplateLoggingInterceptor (#114)

--- src/main/java/org/eclipse/xpanse/tofu/maker/config/RestTemplateConfig.java
+++ src/main/java/org/eclipse/xpanse/tofu/maker/config/RestTemplateConfig.java
@@ -5,6 +5,9 @@
 
 package org.eclipse.xpanse.tofu.maker.config;
 
+import jakarta.annotation.Resource;
+import java.util.Collections;
+import org.eclipse.xpanse.tofu.maker.logging.RestTemplateLoggingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -15,10 +18,14 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class RestTemplateConfig {
 
+    @Resource RestTemplateLoggingInterceptor restTemplateLoggingInterceptor;
+
     /** Create RestTemplate to IOC. */
     @Bean
     public RestTemplate restTemplate(ClientHttpRequestFactory factory) {
-        return new RestTemplate(factory);
+        RestTemplate restTemplate = new RestTemplate(factory);
+        restTemplate.setInterceptors(Collections.singletonList(restTemplateLoggingInterceptor));
+        return restTemplate;
     }
 
     /** Create ClientHttpRequestFactory to IOC. */
